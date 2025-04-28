6dd6ad7e2e35a84ec1ba449ad97066b509883546
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2025-04-28 11:46:00 -0500

    refactor: restructure project file organization

--- src/main/java/com/develop/users_microservice/infrastructure/repository/UserRepositoryImpl.java
+++ src/main/java/com/develop/users_microservice/infrastructure/repository/UserRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.develop.users_microservice.infrastructure.repository;
+import com.develop.users_microservice.domain.model.Role;
 import com.develop.users_microservice.domain.model.User;
 import com.develop.users_microservice.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,18 +10,43 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepositoryImpl  extends JpaRepository<User, Long>, UserRepository{
-    Optional<User> findByEmail(String email);
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepository{
+
+    private final JpaUserRepository jpaUserRepository;
 
     @Override
-    List<User> findAll();
+    public List<User> findAll() {
+        return jpaUserRepository.findAll();
+    }
 
     @Override
-    Optional<User> findById(Long id);
+    public List<User> findByEnabled(boolean enabled) {
+        return jpaUserRepository.findByEnabled(enabled);
+    }
 
     @Override
-    User save(User user);
+    public Optional<User> findById(Long id) {
+        return jpaUserRepository.findById(id);
+    }
 
     @Override
-    void deleteById(Long id);
+    public User save(User user) {
+        return jpaUserRepository.save(user);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jpaUserRepository.deleteById(id);
+    }
+
+    @Override
+    public List<User> findWithFilters(String name, String email, Boolean enabled, Long roleId) {
+        return jpaUserRepository.findWithFilters(name, email, enabled, roleId);
+    }
+
+    @Override
+    public Optional<Role> findRoleById(Long roleId) {
+        return jpaUserRepository.findRoleById(roleId);
+    }
 }
